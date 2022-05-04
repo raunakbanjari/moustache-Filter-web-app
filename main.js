3fa55fbce1fd9a3a3e2e3cce916fe38f06f69875
raunakbanjari <88834316+raunakbanjari@users.noreply.github.com>
2022-05-04 22:23:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.position(500,200)
+    background("white");
+    video = createCapture(VIDEO)
+    video.size(300,300);
+    video.hide();
+
+   
+}
+function draw(){
+
+}
