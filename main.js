5f683fdae342117998cc5132d52119e72c791e42
raunakbanjari <88834316+raunakbanjari@users.noreply.github.com>
2022-05-04 22:50:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
+noseX = 0
+noseY = 0
 function preload(){
 
 }
 function setup(){
     canvas = createCanvas(300,300);
-    canvas.position(500,200)
+    canvas.position(500,200);
     background("white");
-    video = createCapture(VIDEO)
+    video = createCapture(VIDEO);
     video.size(300,300);
     video.hide();
 
-   
+   posenet = ml5.poseNet(video,modelLoaded);
+   posenet.on('pose', gotPoses);
 }
 function draw(){
 
+}
+function modelLoaded(){
+    console.log('PoseNet is initialized!');
+    
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+   
+    }
 }
